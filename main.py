663691c433e55828d4dcd35322b12799472dc2ff
Sumiran Sharma <110280317+Sumi451@users.noreply.github.com>
2025-11-06 12:42:01 +0000

    On branch testit  Your branch is up to date with 'origin/testit'.
     Changes to be committed:	modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-def calculate_sum(a, b):
-    '''Calculate sum of two numbers'''
-    result = a + b
-    return result
+"""
+Simple example file that intentionally includes lint and security issues.
+- Flake8 should flag style issues (unused imports/variables, long lines, import formatting).
+- Bandit should flag insecure uses (pickle.load, os.system / shell=True style usage, hardcoded password).
 
-def divide(a,b):
-    return a/0
+This file is safe as long as you DON'T execute it. It's provided for static analysis only.
+"""
+
+import os, sys, pickle
 
 
+def insecure_function(data_path):
+    # Hardcoded credential (Bandit: B105)
+    secret = "s3cr3t_password"
+
+    # Open file and use pickle (Bandit: unsafe deserialization)
+    f = open(data_path,'rb')
+    data = pickle.load(f)
+    f.close()
+
+    # Use os.system with user data - potential shell injection (Bandit: B602/B204)
+    os.system("ls -la %s" % data_path)
+
+    # intentionally long line to trigger flake8 E501
+    print("This is a very long line that will exceed typical line-length limits set by flake8 to demonstrate a style warning from Flake8 tools.")
+
+    return data
+
+
+def helper():
+    unused_var = 42
+    return None
